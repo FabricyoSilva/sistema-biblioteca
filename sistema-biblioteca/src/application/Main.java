1665bff57fe52a548f1a3ccdd134b37db461404b
Fabricyo Silva <fabricyoclash419@gmail.com>
2026-03-06 18:12:56 -0300

    definir quais ações podemos fazer com um Livro no banco com o arquivo ILirvroDao na pasta interfaces |  Teste do funcionanmento do banco de dados na main

--- sistema-biblioteca/src/application/Main.java
+++ sistema-biblioteca/src/application/Main.java
@@ -1,6 +1,19 @@
 package application;
 
+import application.util.ConnectionFactory;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Tentando conectar ao banco de dados...");
+        try (Connection conexao = ConnectionFactory.getConnection()) {
+            if (conexao != null) {
+                System.out.println("Sucesso!");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro na conexao");
+            e.printStackTrace();
+        }
     }
 }
---
+++ sistema-biblioteca/src/application/interfaces/ILivroDao.java
@@ -0,0 +1,7 @@
+package application.interfaces;
+
+import application.model.Livro;
+
+public interface ILivroDao {
+    void salvar(Livro livro);
+}
