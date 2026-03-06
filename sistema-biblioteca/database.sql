216007f64aa1b099c5a7fc85d03cf070dfcb6c48
Fabricyo Silva <fabricyoclash419@gmail.com>
2026-03-06 17:42:44 -0300

    Criação do banco de dados e da entidade livro

---
+++ sistema-biblioteca/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE livros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    ano_publicacao INT,
+    disponivel BOOLEAN DEFAULT TRUE
+);
---
+++ sistema-biblioteca/src/model/Livro.java
@@ -0,0 +1,72 @@
+package model;
+
+public class Livro {
+    private Integer id;
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean diponivel;
+
+    public Livro() {
+
+    }
+
+    public Livro(Integer id, String titulo, String autor, int anoPublicacao, boolean diponivel) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.diponivel = diponivel;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean isDiponivel() {
+        return diponivel;
+    }
+
+    public void setDiponivel(boolean diponivel) {
+        this.diponivel = diponivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                ", diponivel=" + diponivel +
+                '}';
+    }
+}
